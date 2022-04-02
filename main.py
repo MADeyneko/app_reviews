a67031e0000aa2ee60a7f7468fc22c2e4c669f9d
Maxim Deyneko <madeyneko@gmail.com>
2022-04-02 22:44:51 +0800

    First work version

--- main.py
+++ main.py
@@ -1,66 +1,111 @@
-import selen
+import codecs
+import requests
+import threading
+from queue import Queue
+import pandas as pd
+import time
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import progressbar
+import math 
+import os
+import sys
+from bs4 import BeautifulSoup
+from lxml import html
+import urllib
+from datetime import datetime
 
-# import sqlite3
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# import requests
-# from selenium.webdriver.common.by import By
-# import csv
+class parse_html:
 
+    def __init__(self) -> None:
+        self.name=''
+    
+    def get_rewviews(self,name):
+        self.name=name
+        text=self.get_text_file()
+        soup = BeautifulSoup(text)
+        result=pd.DataFrame(columns = ['Кто','Оценка','Дата','Текст','Ответ'])
+        all_reviews=soup.find_all('div', {'class': 'd15Mdf bAhLNe'})
+        
+        for review in all_reviews:
+            who=review.find('span',{'class': 'X43Kjb'}).text
+            star=review.find('div',{'class':'pf5lIe'}).contents[0]['aria-label'].replace('Средняя оценка: ','')
+            date=review.find('span',{'class':'p2TkOb'}).text.replace(' г.', '')
+            date=datetime(self.get_year(date), self.get_month(date), self.get_day(date))
+            text=review.find('span',{'jsname':'fbQN7e'})
+            if text == None :
+                text =review.text 
 
+            elif text.text == '':
+                text=review.find('span',{'jsname':'bN97Pc'}).text               
+            else:
+                text=review.find('span',{'jsname':'fbQN7e'}).text
 
-# def pars_link():
-#     reader_object = pup()
-
-
-#     with open('bruh12.csv', 'w', encoding="UTF8") as fi:
-#         file_writer = csv.writer(fi, delimiter="|", lineterminator="\r")
-
-#         # file_writer.writerow(["title", "tipe", "data_question", "question", "answer", "date_answer", "link"])
-
-#         for i in reader_object:
-#             print(i)
-
-#             options = Options()
-#             options.add_argument('--headless')
-#             options.add_argument('--disable-gpu')
-
-#             driver = webdriver.Chrome(executable_path=r"/user/deyneko-ma/repo/app_reviews/chromedriver", options=options)
-#             driver.get("https://www.banki.ru" + i[0])
-
-#             title = driver.find_elements(By.CLASS_NAME, "l5474aaa7")
-#             tipe = driver.find_elements(By.CLASS_NAME, "lf1f886e6")
-#             data = driver.find_elements(By.ID, "faq-date-time-2")
-#             question = driver.find_elements(By.CLASS_NAME, "OdPqK")
-#             answer = driver.find_elements(By.CLASS_NAME, "dmfsla")
-#             date_answer = driver.find_elements(By.CLASS_NAME, "MIkZT")
-#             link = "https://www.banki.ru" + i[0]
-
-#             try:
-#                 file_writer.writerow(
-#                     [title[0].text, tipe[0].text, data[0].text, question[0].text, answer[0].text, date_answer[0].text, link])
-#                 print(title[0].text, tipe[0].text, data[0].text, question[0].text, answer[0].text, date_answer[0].text,
-#                       link)
-#             except:
-#                 file_writer.writerow([title[0].text, tipe[0].text, data[0].text, question[0].text, "None", "None", link])
-#                 print(title[0].text, tipe[0].text, data[0].text, question[0].text, "None", "None", link)
-#             finally:
-#                 continue
+            ans=''
 
 
-# def pup():
-#     red = []
-#     line_count = 0
-#     with open('bruh.csv', 'r') as f:
-#         reader_object = csv.reader(f, delimiter=",")
+            result = pd.concat(
+                    [result,
+                    pd.DataFrame([
+                        [who, star, date, text, ans]]
+                        ,columns = ['Кто','Оценка','Дата','Текст','Ответ']
+                        )
+                    ]
+                )
+            
+    
 
-#         for i in reader_object:
-#             if line_count == 1:
-#                 red.append(i)
-#             else:
+        return result
+            # print(1)
 
-#                 line_count += 1
-#     return red
-# # sel()
-# pars_link()
+    def get_year(self, d):
+        d=d.split(' ')
+        return int(d[2])
+
+    def get_month(self, d):
+        unit_to_multiplier = {
+            'января': 1,
+            'февраля': 2,
+            'марта': 3,
+            'апреля': 4,
+            'мая': 5,
+            'июня': 6,
+            'июля': 7,
+            'августа': 8,
+            'сентября': 9,
+            'октября': 10,
+            'ноября': 11,
+            'декабря': 12,
+        }
+        d=d.split(' ')
+        
+        return int(unit_to_multiplier[d[1]])
+
+    def get_day(self, d):
+        d=d.split(' ')
+        return int(d[0])
+
+    def get_text_file(self):
+        # return open(self.name,'r').read()
+        fileObj = codecs.open( self.name, "r", "utf_8_sig" )
+        text = fileObj.read() # или читайте по строке
+        fileObj.close()
+
+        return text
+
+if __name__=='__main__':
+    res=pd.DataFrame()
+    parse=parse_html()
+    
+    res=pd.concat(
+        [
+            res
+            ,parse.get_rewviews('rev_1.html')
+            ,parse.get_rewviews('rev_2.html')
+            ,parse.get_rewviews('rev_3.html')
+            ,parse.get_rewviews('rev_4.html')
+            ,parse.get_rewviews('rev_5.html')
+        ]
+    )
+
+    res.to_excel('test.xlsx', index=False)
